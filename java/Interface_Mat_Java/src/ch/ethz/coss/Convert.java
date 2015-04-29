fb22964cad6b3ddacb7b84ee67c9e566943908a6
bbinoy <beb@posteo.de>
2015-04-29 14:31:53 +0200

    Created java class file for converting our format to .m format

---
+++ java/Interface_Mat_Java/src/ch/ethz/coss/Convert.java
@@ -0,0 +1,13 @@
+package ch.ethz.coss;
+
+public class Convert {
+	public Convert () {
+		System.out.println("Give valid arguments.");
+	}
+	public Convert (String folderpath) {
+		run(folderpath);
+	}
+	private void run(String folderpath) {
+		
+	}
+}
