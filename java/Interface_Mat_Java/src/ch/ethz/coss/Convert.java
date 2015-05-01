6a2f2439d3c854992f5e4dbe2d89b5461743e4f2
bbinoy <beb@posteo.de>
2015-05-01 16:43:10 +0200

    Conversion tool from SFINA to Matpower format finished. Implemented isolated status of buses as well. Not yet testet against original matpower case file.

--- java/Interface_Mat_Java/src/ch/ethz/coss/Convert.java
+++ java/Interface_Mat_Java/src/ch/ethz/coss/Convert.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * 
@@ -14,9 +15,12 @@ import java.nio.file.Paths;
  *
  */
 public class Convert {
-	private static String bus = "";
-	private static String gen = "";
-	private static String bra = "";
+	private static ArrayList<ArrayList<String>> bus_data = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> bra_data = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> bra_meta_data = new ArrayList<ArrayList<String>>();
+	private static String bus_str = "";
+	private static String gen_str = "";
+	private static String bra_str = "";
 	
 	/**
 	 * Runs the conversion from SFINA custom .txt to Matpower .m format. 
@@ -34,8 +38,80 @@ public class Convert {
 		BufferedReader read_branch = Files.newBufferedReader(Paths.get(branch_path), encoding);
 		BufferedReader read_branch_meta = Files.newBufferedReader(Paths.get(branch_meta_path), encoding);
 		
-		extract(read_bus, read_branch, read_branch_meta);
+		extractData(read_bus, read_branch, read_branch_meta);
+		makeMatStrings();
+		writeToMatFile(writer, case_name);
 		
+		writer.close();
+		read_bus.close();
+		read_branch.close();
+		System.out.println("Conversion successful.");
+	}
+	
+	private static void extractData(BufferedReader read_bus, BufferedReader read_branch, BufferedReader read_branch_meta) throws IOException {
+		String line_bus = null;
+		String line_bra = null;
+		String line_bra_m = null;
+		int line_counter = 0;
+		
+		while ((line_bus = read_bus.readLine()) != null) {
+			String[] split = line_bus.split(",");
+			bus_data.add(new ArrayList<String>());
+			for (int i = 0; i < split.length; i++) { 
+				bus_data.get(line_counter).add(split[i]);
+			}
+			line_counter++;
+		}
+		
+		line_counter = 0;
+		while ((line_bra = read_branch.readLine()) != null && (line_bra_m = read_branch_meta.readLine()) != null ) {
+			String[] split_bra = line_bra.split(",");
+			String[] split_bra_m = line_bra_m.split(",");
+			bra_data.add(new ArrayList<String>());
+			bra_meta_data.add(new ArrayList<String>());
+			for (int i = 0; i < split_bra.length; i++){
+				bra_data.get(line_counter).add(split_bra[i]);
+			}
+			for (int i = 0; i < split_bra_m.length; i++){
+				bra_meta_data.get(line_counter).add(split_bra_m[i]);
+			}
+			line_counter++;
+		}
+	}
+	
+	private static void makeMatStrings() {
+		// add bus values to bus string
+		for (int i = 1; i < bus_data.size(); i++){
+			for (int j = 0; j < 13; j++){
+				if (j == 1) bus_str += "	" + getBusType(i);
+				else bus_str += "	" + bus_data.get(i).get(j);
+			}
+			bus_str += ";\n";
+			// pick out generators and add their id and additional values in generator string
+			if (bus_data.get(i).get(1).equals("GEN")) {
+				gen_str += "	" + bus_data.get(i).get(0);
+				for (int j = 13; j < bus_data.get(i).size(); j++) gen_str += "	" + bus_data.get(i).get(j);
+				gen_str += ";\n";
+			}
+		}
+
+		// add branch values to branch string
+		for (int i = 1; i < bra_data.size(); i++){
+			for (int j = 1; j < 13; j++){
+				if (j == 1 || j == 2) bra_str += "	" + bra_data.get(i).get(j); // from_bus and to_bus
+				else if (j == 10) bra_str += "	" + bra_meta_data.get(i).get(j) + "	" + invertBranchStatus(bra_data.get(i).get(3)); // include status of line
+				else bra_str += "	" + bra_meta_data.get(i).get(j); // all other branch info
+			}
+			bra_str += ";\n";
+		}		
+	}
+	
+	private static String invertBranchStatus(String branch_status) {
+		if (branch_status.equals("1")) return "0";
+		else return "1";
+	}
+	
+	private static void writeToMatFile(BufferedWriter writer, String case_name) throws IOException {
 		// Write header information in .m file
 		writer.write("function mpc = " + case_name.toString() + "\n");
 		writer.write("% Case file generated from SFINA file format\n");
@@ -44,67 +120,45 @@ public class Convert {
 		writer.newLine();
 		writer.write("%%-----  Power Flow Data  -----%%\n%% system MVA base\nmpc.baseMVA = 100;\n");
 		writer.newLine();
-		
+				
 		// Write bus data
 		writer.write("%% bus data\n%	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin\nmpc.bus = [\n");
-		writer.write(bus);
+		writer.write(bus_str);
 		writer.write("];\n");
 		
 		// Write generator data
 		writer.write("%% generator data\n%	bus	Pg	Qg	Qmax	Qmin	Vg	mBase	status	Pmax	Pmin	Pc1	Pc2	Qc1min	Qc1max	Qc2min	Qc2max	ramp_agc	ramp_10	ramp_30	ramp_q	apf\nmpc.gen = [\n");
-		writer.write(gen);
+		writer.write(gen_str);
 		writer.write("];\n");
-		
+				
 		// Write branch data
 		writer.write("%% branch data\n%	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status	angmin	angmax\nmpc.branch = [\n");
-		writer.write(bra);
+		writer.write(bra_str);
 		writer.write("];\n");
-		
-		writer.close();
-		read_bus.close();
-		read_branch.close();
-		System.out.println("Conversion successful.");
 	}
-	private static void extract(BufferedReader read_bus, BufferedReader read_branch, BufferedReader read_branch_meta) throws IOException {
-		String line_bus = null;
-		String line_bra = null;
-		String line_bra_m = null;
-		while ((line_bus = read_bus.readLine()) != null) {
-			String[] split = line_bus.split(",");
-			if (split[0].equals("node_id")) continue; // skip labels of first line
-			for (int i = 0; i < 13; i++) { // extract bus info and append it to variable bus
-				if (i == 1) { // sort out the type of bus
-					if (split[1].equals("SLACK_BUS")) bus += "	3";
-					else if (split[1].equals("GEN")) bus += "	2";
-					else if (split[1].equals("BUS")) bus += "	1";
-					else { // catch if bus type is not recognized
-						bus += "	NaN";
-						System.out.println("Problem: Didn't recognize bus type! node_id = " + split[0].toString());
-					}
-				}
-				else 
-					bus += "	" + split[i];
+	
+	private static boolean isIsolated(int bus_row) {
+		String bus = bus_data.get(bus_row).get(0);
+		boolean is_isolated = true;
+		for (int i = 1; i < bra_data.size(); i++){
+			if (bra_data.get(i).get(1).equals(bus) || bra_data.get(i).get(2).equals(bus) && bra_data.get(i).get(3).equals("0")) {
+				is_isolated = false;
 			}
-			bus += ";\n"; // end of line
+		}
+		return is_isolated;
+	}
 
-			if (split[1].equals("GEN")) { // extract which buses are generators and append their additional info to variable gen
-				gen += split[0];
-				for (int i = 13; i < split.length; i++) gen += "	" + split[i];
-				gen += ";\n";
-			}
+	private static String getBusType(int bus_row){
+		String type = bus_data.get(bus_row).get(1);
+		String type_matpower = null;
+		if (isIsolated(bus_row)) type_matpower = "4";
+		else if (type.equals("SLACK_BUS")) type_matpower = "3";
+		else if (type.equals("GEN")) type_matpower = "2";
+		else if (type.equals("BUS")) type_matpower = "1";
+		else { // catch if bus type is not recognized
+			type_matpower = "NaN";
+			System.out.println("Problem: Didn't recognize bus type!");
 		}
-		while ((line_bra = read_branch.readLine()) != null && (line_bra_m = read_branch_meta.readLine()) != null ) {
-			String[] split_bra = line_bra.split(",");
-			String[] split_bra_m = line_bra_m.split(",");
-			if (split_bra[0].equals("branch_id")) continue; // skip labels of first line
-			bra += "	" + split_bra[1]; // from_bus
-			bra += "	" + split_bra[2]; // to_bus
-			for (int i = 3; i < split_bra_m.length - 2; i++) {
-				bra += "	" + split_bra_m[i];
-			}
-			bra += "	" + split_bra[3]; // status
-			bra += "	" + split_bra_m[split_bra_m.length - 2] + "	" + split_bra_m[split_bra_m.length - 1]; // angmin and angmax  
-			bra += ";\n"; 
-		}
+		return type_matpower;
 	}
 }
