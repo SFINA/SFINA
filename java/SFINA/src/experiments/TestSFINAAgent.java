2511e7e2b7978d6ae40c6c7a1e69727e808c67f7
epournaras <evangelospournaras@public-docking-rz-1117.ethz.ch>
2016-01-22 16:52:22 +0100

    Loading system parameters from file.

--- java/SFINA/src/experiments/TestSFINAAgent.java
+++ java/SFINA/src/experiments/TestSFINAAgent.java
@@ -22,7 +22,10 @@ import input.Backend;
 import input.Domain;
 import input.SystemParameter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import power.PowerFlowType;
 import protopeer.Experiment;
 import protopeer.Peer;
@@ -47,8 +50,6 @@ public class TestSFINAAgent extends SimulatedExperiment{
     private final static int N=1;
     
     // SFINA parameters
-    private final static HashMap<SystemParameter,Object> systemParameters = new HashMap();
-    
     private final static String columnSeparator=",";
     private final static String missingValue="-";
     
@@ -62,20 +63,23 @@ public class TestSFINAAgent extends SimulatedExperiment{
     private final static String experimentConfigurationFilesLocation=configurationFilesLocation+experimentID+inputDirectoryName+"/";
     private final static String experimentOutputFilesLocation=configurationFilesLocation+experimentID+outputDirectoryName+"/";
     private final static String eventsLocation=experimentConfigurationFilesLocation+"/events.txt";
+    private final static String systemParametersLocation=experimentConfigurationFilesLocation+"/system-parameters.txt";
     private final static String nodesLocation="/"+topologyDirectoryName+"/nodes.txt";
     private final static String linksLocation = "/"+topologyDirectoryName+"/links.txt";
     private final static String nodesFlowLocation ="/"+flowDirectoryName+"/nodes.txt";
     private final static String linksFlowLocation ="/"+flowDirectoryName+"/links.txt";
     
     public static void main(String[] args) {
-        // Necessary
-        systemParameters.put(SystemParameter.DOMAIN, Domain.POWER);
-        systemParameters.put(SystemParameter.BACKEND, Backend.MATPOWER);
-        systemParameters.put(SystemParameter.FLOW_TYPE, PowerFlowType.AC);
-        
-        // Optional, not yet implemented to afffect anything
-        systemParameters.put(SystemParameter.TOLERANCE_PARAMETER, 2.0);
-        systemParameters.put(SystemParameter.CAPACITY_CHANGE, 0.0);
+        HashMap<SystemParameter,Object> systemParameters=loadSystemParameters(systemParametersLocation,columnSeparator);
+        System.out.println(systemParameters);
+//        // Necessary
+//        systemParameters.put(SystemParameter.DOMAIN, Domain.POWER);
+//        systemParameters.put(SystemParameter.BACKEND, Backend.MATPOWER);
+//        systemParameters.put(SystemParameter.FLOW_TYPE, PowerFlowType.AC);
+//        
+//        // Optional, not yet implemented to afffect anything
+//        systemParameters.put(SystemParameter.TOLERANCE_PARAMETER, 2.0);
+//        systemParameters.put(SystemParameter.CAPACITY_CHANGE, 0.0);
         
         System.out.println("Experiment "+expSeqNum+"\n");
         Experiment.initEnvironment();
@@ -127,5 +131,82 @@ public class TestSFINAAgent extends SimulatedExperiment{
             }
         }
         experiment.delete();
+    }
+    
+    public final static HashMap<SystemParameter,Object> loadSystemParameters(String location, String separator){
+        HashMap<SystemParameter,Object> systemParameters = new HashMap();
+        File file = new File(location);
+        Scanner scr = null;
+        try {
+            scr = new Scanner(file);
+            while(scr.hasNext()){
+                StringTokenizer st = new StringTokenizer(scr.next(), separator);
+                switch(st.nextToken()){
+                    case "domain":
+                        Domain domain=null;
+                        String domainType=st.nextToken();
+                        switch(domainType){
+                            case "power":
+                                domain=Domain.POWER;
+                                break;
+                            case "gas":
+                                domain=Domain.GAS;
+                                break;
+                            case "water":
+                                domain=Domain.WATER;
+                                break;
+                            case "transportation":
+                                domain=Domain.TRANSPORTATION;
+                                break;
+                            default:
+                                logger.debug("This domain is not supported or cannot be recognized");
+                        }
+                        systemParameters.put(SystemParameter.DOMAIN, domain);
+                        break;
+                    case "backend":
+                        Backend backend=null;
+                        String backendType=st.nextToken();
+                        switch(backendType){
+                            case "MATPOWER":
+                                backend=Backend.MATPOWER;
+                                break;
+                            case "InterPSS":
+                                backend=Backend.INTERPSS;
+                                break;
+                            default:
+                                logger.debug("This backend is not supported or cannot be recognized");
+                        }
+                        systemParameters.put(SystemParameter.BACKEND, backend);
+                        break;
+                    case "flow_type":
+                        PowerFlowType powerFlow=null;
+                        String powerFlowType=st.nextToken();
+                        switch(powerFlowType){
+                            case "DC":
+                                powerFlow=PowerFlowType.DC;
+                                break;
+                            case "AC":
+                                powerFlow=PowerFlowType.AC;
+                                break;
+                            default:
+                                logger.debug("This flow type is not supported or cannot be recognized");
+                        }
+                        systemParameters.put(SystemParameter.FLOW_TYPE, powerFlow);
+                        break;
+                    case "tolerance_parameter":
+                        systemParameters.put(SystemParameter.TOLERANCE_PARAMETER, Double.parseDouble(st.nextToken()));
+                        break;
+                    case "capacity_change":
+                        systemParameters.put(SystemParameter.CAPACITY_CHANGE, Double.parseDouble(st.nextToken()));
+                        break;
+                    default:
+                        logger.debug("This system parameter is not supported or cannot be recognized");
+                }
+            }
+        }
+        catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }
+        return systemParameters;
     }
 }
