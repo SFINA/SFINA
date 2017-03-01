4a9b60a5342a1ce2bac80e6d07b169edee566f4d
bbinoy <bbrandt@student.ethz.ch>
2017-03-01 10:37:34 +0100

    Move libraries to own repo, add backendparameterslaoderinterface
    Also deleted some unused old classes

--- core/src/backend/BackendParameterLoaderInterface.java
+++ core/src/backend/BackendParameterLoaderInterface.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 SFINA Team
+ * Copyright (C) 2017 SFINA Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -15,22 +15,17 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package utilities;
+package backend;
+
+import java.util.HashMap;
 
 /**
  *
- * @author evangelospournaras
+ * @author Ben
  */
-public enum Metrics {
-    LINE_FLOW,
-    LINE_UTILIZATION,
-    ACTIVATED_LINES,
-    TOTAL_LINES,
-    NODE_FINAL_LOADING,
-    NODE_INIT_LOADING,
-    FLOW_SIMU_TIME,
-    TOT_SIMU_TIME,
-    NEEDED_ITERATIONS,
-    ISLANDS,
-    ISOLATED_NODES
+public interface BackendParameterLoaderInterface { 
+        
+    public HashMap<Enum,Object> loadBackendParameters(String location);
+
+    
 }
